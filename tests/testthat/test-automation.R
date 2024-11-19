54c4bc75e14a562c0512ae581acc6417fc47ef34
Toryn Schafer <toryn27@gmail.com>
2024-11-19 11:39:14 -0600

    added on test for warning on default inputs

---
+++ tests/testthat/test-automation.R
@@ -0,0 +1,11 @@
+###############################################
+################### TESTING ###################
+###############################################
+
+dataframe <- data.frame(x = 1:20,
+                        y = 20:1)
+
+test_that("data.frame.lag.lead works with default inputs", {
+  expect_warning(data.frame.lag.lead(dataframe, covariates = list("x")))
+})
+
